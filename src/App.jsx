4908161c028c7ba63180fbe02a1e2aa587ab0990
SomMariya <m.som-2030@kpi.ua>
2021-12-18 01:31:40 +0200

    deleted sign-in button

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import SwaggerUI from 'swagger-ui-react';
 import { Navbar, Container, Nav } from 'react-bootstrap';
-import Alert from 'react-bootstrap/Alert'
+import Alert from 'react-bootstrap/Alert';
 import { useState, useEffect } from 'react';
 import { GoogleLogin, GoogleLogout } from 'react-google-login';
 import UserService from './services/user.service';
@@ -14,7 +14,6 @@ function App() {
   const [show, setShow] = useState(false);
 
   useEffect(() => {
-    // console.log(userData);
     if (userData.accessKey && userData.username) {
       setSignedIn(true);
     }
@@ -24,17 +23,22 @@ function App() {
 
   const onUserLogIn = async (googleResponse) => {
     const { accessKey, username } = await userService.createUser(googleResponse);
-    const { tokenId, googleId } = googleResponse
+    const { tokenId, googleId } = googleResponse;
     setUserData({ accessKey, username, tokenId, googleId });
   };
 
   const onUpdateAccessKey = async () => {
-    const response = await userService.updateAccessKey(userData.username, userData.tokenId, userData.googleId);
-    if(response.name=='Error'){
-     setShow(true)
-    }else{
-    const accessKey= response.accessKey;
-    setUserData({ ...userData, accessKey});}
+    const response = await userService.updateAccessKey(
+      userData.username,
+      userData.tokenId,
+      userData.googleId
+    );
+    if (response.name == 'Error') {
+      setShow(true);
+    } else {
+      const accessKey = response.accessKey;
+      setUserData({ ...userData, accessKey });
+    }
   };
 
   const onUserLogOut = () => {
@@ -63,13 +67,7 @@ function App() {
                   />
                 </>
               ) : (
-                <GoogleLogin
-                  clientId="759950908416-hdi0alconb3krbcblsmidir074uvafoj.apps.googleusercontent.com"
-                  buttonText="Sign in"
-                  onSuccess={onUserLogIn}
-                  onFailure={onUserLogIn}
-                  cookiePolicy={'single_host_origin'}
-                />
+                <></>
               )}
             </Nav>
           </Navbar.Collapse>
@@ -91,30 +89,31 @@ function App() {
             />
           ) : (
             <>
-              <button className="btnToClipboard"  onClick={() => {
-                  navigator.clipboard.writeText(userData.accessKey);
-                }}>
-                &#x274F;
-              </button>
               <button
                 className="btnToClipboard"
-                onClick={onUpdateAccessKey}
+                onClick={() => {
+                  navigator.clipboard.writeText(userData.accessKey);
+                }}
               >
+                &#x274F;
+              </button>
+              <button className="btnToClipboard" onClick={onUpdateAccessKey}>
                 &#8635;
               </button>
             </>
           )}
         </div>
       </div>
-      {show==true? (
+      {show == true ? (
         <Alert variant="danger" onClose={() => setShow(false)} dismissible>
           <Alert.Heading>Oops!</Alert.Heading>
           <p>You should sign in again</p>
         </Alert>
-        ):<></>}
+      ) : (
+        <></>
+      )}
       <SwaggerUI url="https://measurment-assistant.herokuapp.com/documentation/json" />
     </div>
-    
   );
 }
 
